7bed12949be9df332a12e391535326334e7bd91b
soy Alan <anto211970@gmail.com>
2024-03-18 21:07:50 -0600

    primer proyecto

---
+++ ProyectoLogin/resources/scrip.js
@@ -0,0 +1,12 @@
+function collapseContainer() {
+    document.getElementById('container').classList.add('collapsed');
+}
+
+function expandContainer() {
+    document.getElementById('container').classList.remove('collapsed');
+}
+
+function collapseContainer() {
+    // Lógica para colapsar el contenedor aquí
+}
+
